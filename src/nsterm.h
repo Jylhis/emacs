23f8436ad3f23b01937a840bb82fbdcab6353f34
Alan Third <alan@idiocy.org>
2026-05-07 22:57:13 +0100

    [NS] Fix deprecated variable (bug#80985)
    * src/nsterm.h (NSLevelIndicatorStyleContinuousCapacity): Define in
    macOS < 10.15.
    
    (cherry picked from commit 543d8a7a9d7edadced77aced028b848bd9802a6b)

--- src/nsterm.h
+++ src/nsterm.h
@@ -1319,6 +1319,11 @@ enum NSWindowTabbingMode
 #define NSButtonTypeMomentaryPushIn NSMomentaryPushInButton
 #endif
 
+#if !defined (NS_IMPL_COCOA) || !defined (MAC_OS_X_VERSION_10_15)
+/* Deprecated in macOS 10.15.  */
+#define NSLevelIndicatorStyleContinuousCapacity NSContinuousCapacityLevelIndicatorStyle
+#endif
+
 extern void mark_nsterm (void);
 
 #endif	/* HAVE_NS */
